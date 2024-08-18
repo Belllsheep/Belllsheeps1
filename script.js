7735e166486e55e890868ba972c20744301c337d
Belllsheep <1139979291@qq.com>
2024-08-18 22:45:21 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('changeColorButton').addEventListener('click', function() {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#33FFF5'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
